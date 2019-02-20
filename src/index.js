41937379537e5ccf90d9792fae3bf932ab081941
Nikita Plakhin <nplkhn1@yandex.ru>
2019-02-21 00:52:08 +0300

    first working version. 12/20 tests

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
   let closinBracket = 0;
   let openingBracket = 0;
-  for (let i = 0; i < bracketsConfig.lenght; i++){
-    for (let j = 0; j < str.lenght; j++){
+  for (let j = 0; j < str.length; j++){
+    for (let i = 0; i < bracketsConfig.length; i++){
       if (str[j] == bracketsConfig[i][0]){
         openingBracket++;
-      } else if (str[j] == bracketsConfig[i][0]){
+      } else if (str[j] == bracketsConfig[i][1]){
         closinBracket++;
       }
-    }
-    if (closinBracket != openingBracket){
-      return false;
-    }
-    closinBracket = 0;
-    openingBracket = 0;
+      if (closinBracket > openingBracket){
+        return false;
+      }
+    }  
+  }
+  if (closinBracket != openingBracket){
+    return false;
   }
   return true;
 }
