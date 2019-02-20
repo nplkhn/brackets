2fa4eae1a31218b979d7789161f2d7a725c8d354
Nikita Plakhin <nplkhn1@yandex.ru>
2019-02-20 13:09:41 +0300

    11/20

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let closinBracket = 0;
+  let openingBracket = 0;
+  for (let i = 0; i < bracketsConfig.lenght; i++){
+    for (let j = 0; j < str.lenght; j++){
+      if (str[j] == bracketsConfig[i][0]){
+        openingBracket++;
+      } else if (str[j] == bracketsConfig[i][0]){
+        closinBracket++;
+      }
+    }
+    if (closinBracket != openingBracket){
+      return false;
+    }
+    closinBracket = 0;
+    openingBracket = 0;
+  }
+  return true;
 }
