2d404e9d563be2b250c20e7b0b3044d8c41ec4c5
Nikita Plakhin <nplkhn1@yandex.ru>
2019-02-21 16:06:29 +0300

    solution by stack

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  let brackets = 0;
-  for (let j = 0; j < str.length; j++) {
-    for (let i = 0; i < bracketsConfig.length; i++) {
-      if (bracketsConfig[i][0] == bracketsConfig[i][1]) {
-        brackets++;
-        brackets--;
-      } else if (str[j] == bracketsConfig[i][0]) {
-        brackets++;
-      } else if (str[j] == bracketsConfig[i][1]) {
-        brackets--;
-      }
-      if (brackets < 0) {
+  let stack = [];
+  let first = true;
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (bracketsConfig[j][0] == bracketsConfig[j][1]) {
+        if (first) {
+          stack.push(str[i]);
+          first = false;
+        } else {
+          if (str[i] == bracketsConfig[j][1] && stack[stack.length - 1] == bracketsConfig[j][0]) {
+            stack.pop();
+            first = true;
+          }
+        }
+      } else if (str[i] == bracketsConfig[j][0]) {
+        stack.push(str[i]);
+      } else if (str[i] == bracketsConfig[j][1] && stack[stack.length - 1] == bracketsConfig[j][0]) {
+        stack.pop();
+      } else if (str[i] == bracketsConfig[j][1] && stack[stack.length - 1] != bracketsConfig[j][0]) {
         return false;
       }
     }
   }
-  if (brackets != 0) {
+  if (stack.length != 0) {
     return false;
   }
   return true;
