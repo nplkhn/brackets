4a39ac923249339b5ce2b9a4e5701defd44184be
Nikita Plakhin <nplkhn1@yandex.ru>
2019-02-21 12:10:40 +0300

    little changes

--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  let closinBracket = 0;
-  let openingBracket = 0;
-  for (let j = 0; j < str.length; j++){
-    for (let i = 0; i < bracketsConfig.length; i++){
-      if (bracketsConfig[i][0] == bracketsConfig[i][1]){
-        closinBracket++;
-        openingBracket++;
-      } else if (str[j] == bracketsConfig[i][0]){
-        openingBracket++;
-      } else if (str[j] == bracketsConfig[i][1]){
-        closinBracket++;
+  let brackets = 0;
+  for (let j = 0; j < str.length; j++) {
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      if (bracketsConfig[i][0] == bracketsConfig[i][1]) {
+        brackets++;
+        brackets--;
+      } else if (str[j] == bracketsConfig[i][0]) {
+        brackets++;
+      } else if (str[j] == bracketsConfig[i][1]) {
+        brackets--;
       }
-      if (closinBracket > openingBracket){
+      if (brackets < 0) {
         return false;
       }
-    }  
+    }
   }
-  if (closinBracket != openingBracket){
+  if (brackets != 0) {
     return false;
   }
   return true;
 }
 
-//5 8 9 11 12 15 19 20
+//5 10 13 14
