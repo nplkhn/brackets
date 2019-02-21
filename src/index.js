0d3748d2bccecf74a0ba3ab19af51097404ed806
Nikita Plakhin <nplkhn1@yandex.ru>
2019-02-21 11:51:19 +0300

    16/20

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ module.exports = function check(str, bracketsConfig) {
   let openingBracket = 0;
   for (let j = 0; j < str.length; j++){
     for (let i = 0; i < bracketsConfig.length; i++){
-      if (str[j] == bracketsConfig[i][0]){
+      if (bracketsConfig[i][0] == bracketsConfig[i][1]){
+        closinBracket++;
+        openingBracket++;
+      } else if (str[j] == bracketsConfig[i][0]){
         openingBracket++;
       } else if (str[j] == bracketsConfig[i][1]){
         closinBracket++;
@@ -18,3 +21,5 @@ module.exports = function check(str, bracketsConfig) {
   }
   return true;
 }
+
+//5 8 9 11 12 15 19 20
